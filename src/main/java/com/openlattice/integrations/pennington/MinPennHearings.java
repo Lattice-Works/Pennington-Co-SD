8bfbf05f7c3fe24f18ed980857884327f8708a9d
Kim Engie <kim@openlattice.com>
2018-05-01 14:51:04 -0700

    Hearings flight

--- src/main/java/com/openlattice/integrations/pennington/MinPennHearings.java
+++ src/main/java/com/openlattice/integrations/pennington/MinPennHearings.java
@@ -25,10 +25,10 @@ public class MinPennHearings {
     private static final Logger                      logger      = LoggerFactory.getLogger( MinPennHearings.class );
     private static final RetrofitFactory.Environment environment = RetrofitFactory.Environment.LOCAL;
 
-    private static final DateTimeHelper dtHelper = new DateTimeHelper( TimeZones.America_Denver,
+    private static final DateTimeHelper dtHelper = new DateTimeHelper( TimeZones.America_Chicago,
             "MM/dd/yyyy" );
-    private static final DateTimeHelper tHelper = new DateTimeHelper( TimeZones.America_Denver,
-            "hh:mmaa" );
+    private static final DateTimeHelper tHelper = new DateTimeHelper( TimeZones.America_Chicago,
+            "hh:mma" );
 
 
     public static void main( String[] args ) throws InterruptedException, IOException {
@@ -50,11 +50,11 @@ public class MinPennHearings {
                     .addProperty( "nc.PersonBirthDate" )
                         .value( row -> dtHelper.parseDate( row.getAs("InmateDOB") )).ok()
                     .addProperty( "nc.PersonSurName" )
-                        .value( MinPennHearings::getLastName ).ok()
-                    .addProperty( "nc/PersonGivenName" )
-                        .value( MinPennHearings::getFirstName ).ok()
+                        .value( row -> getLastName( row.getAs( "InmateName" )) ).ok()
+                    .addProperty( "nc.PersonGivenName" )
+                        .value( row -> getFirstName (row.getAs( "InmateName" ))).ok()
                     .addProperty( "nc.PersonMiddleName" )
-                        .value( MinPennHearings::getMiddleName ).ok()
+                        .value(  row -> getMiddleName (row.getAs( "InmateName" ))).ok()
                     .addProperty( "nc.PersonSuffix" )
                         .value( MinPennHearings::getSuffix ).ok()
                 .endEntity()
@@ -62,9 +62,11 @@ public class MinPennHearings {
                     .to( "MinPenPeople" )
                     .entityIdGenerator( row -> row.get("JudicialOfficer") )
                     .addProperty( "nc.PersonGivenName" )
-                        .value( MinPennHearings::getFirstName ).ok()
+                        .value( row -> getFirstName (row.getAs( "JudicialOfficer" ))).ok()
                     .addProperty( "nc.PersonSurName" )
-                        .value( MinPennHearings::getLastName ).ok()
+                        .value( row -> getLastName( row.getAs( "JudicialOfficer" ) ) ).ok()
+                    .addProperty( "nc.PersonMiddleName" )
+                        .value(  row -> getMiddleName (row.getAs( "JudicialOfficer" ))).ok()
                 .endEntity()
                 .addEntity( "officer" )
                     .to("MinPennOfficers")
@@ -72,18 +74,22 @@ public class MinPennHearings {
                     .addProperty( "publicsafety.personneltitle" )
                         .value( row -> "Judicial Officer" ).ok()
                     .endEntity()
-                .addEntity( "hearing" )
-                    .to("MinPenHearings")
+                .addEntity( "hearing" )        //As justice.case right now
+                    .to("MinPennHearings")
                     .addProperty( "j.CaseNumberText", "ID" )
-                    .addProperty( "justice.courtcasenumber", "DocketNumber" )
-                    .addProperty( "general.date", "HearingDate" )
+                    .addProperty( "general.date").value( row -> dtHelper.parseDate( row.getAs( "HearingDate" ) ) ).ok()
                     .addProperty( "justice.courtcasetype", "HearingType" )
                     .addProperty( "date.timeOfDay" ).value( row -> tHelper.parseTime( row.getAs( "HearingTime" ) )).ok()
                     .addProperty( "event.comments", "HearingComment" )
                     .addProperty( "ol.update", "UpdateType" )
                 .endEntity()
-                .addEntity( "courtroom" )
-                    .to( "MinPenCourtroom" )
+                .addEntity( "case" )
+                    .to( "MinPennCase")
+                    .addProperty( "j.CaseNumberText", "DocketNumber" )
+                .endEntity()
+                .addEntity( "courtroom" )      //using geo.address
+                    .to( "MinPennCourtroom" )
+                    .addProperty( "location.Address", "Courtroom" )
                     .addProperty( "location.name", "Courtroom" )
                 .endEntity()
 
@@ -94,17 +100,40 @@ public class MinPennHearings {
                     .to( "MinPennAppearsin" )
                     .fromEntity( "inmate" )
                     .toEntity( "hearing" )
+                    .entityIdGenerator( row -> row.get( "ID" ) + row.get ("InmateName") )
+                    .addProperty( "general.stringid", "ID")
+                    .addProperty( "nc.SubjectIdentification", "InmateName" )
+                .endAssociation()
+                .addAssociation( "becomes" )
+                    .to("MinPennBecomes")
+                    .fromEntity( "officerperson" )
+                    .toEntity( "officer" )
+                    .addProperty( "nc.SubjectIdentification", "JudicialOfficer" )
+                .endAssociation()
+                .addAssociation( "appearsin2" )
+                    .to( "MinPennAppearsin" )
+                    .fromEntity( "officerperson" )
+                    .toEntity( "hearing" )
+                    .entityIdGenerator( row -> row.get( "ID" ) + row.get ("JudicialOfficer") )
                     .addProperty( "general.stringid", "ID" )
-                    //NEED NC SUBJECTID
+                    .addProperty( "nc.SubjectIdentification", "JudicialOfficer" )
+                .endAssociation()
+                .addAssociation( "appearsin3" )
+                    .to( "MinPennAppearsin" )
+                    .fromEntity( "hearing" )
+                    .toEntity( "case" )
+                    .addProperty( "general.stringid" )
+                        .value( row -> Parsers.getAsString( row.getAs( "DocketNumber" ) ) + Parsers.getAsString( row.getAs( "ID" ) ) ).ok()
+                .endAssociation()
+                .addAssociation( "occurredat" )
+                    .to( "MinPennOccurredat" )
+                    .fromEntity( "hearing" )
+                    .toEntity( "courtroom" )
+                    .addProperty( "general.stringid", "ID" )
                 .endAssociation()
 
-
-
-
                 .endAssociations()
-
                 .done();
-
 
         Shuttle shuttle = new Shuttle( environment, jwtToken );
         Map<Flight, Payload>  flights = new HashMap<>( 1 );
@@ -114,22 +143,37 @@ public class MinPennHearings {
 
 }
 
-    public static String getLastName (Row row){
-        String all = Parsers.getAsString(row.getAs( "InmateName" )).trim();
+    public static String getLastName (Object obj){
+        String all = Parsers.getAsString( obj );
+        if (all == null) return null;
+        all = all.trim();
         if (StringUtils.isNotBlank( all )){
             String [] namesplit = all.split( "," );
-            String lastname = namesplit[ 0 ];   //1st element in array is always the last name
+            String lastname = namesplit[ 0 ].trim();   //1st element in array is always the last name
                     return lastname;
         }
         return null;
     }
 
+//    public static String getLastNameJudge (Row row){
+//        String all = Parsers.getAsString(row.getAs( "JudicialOfficer" )).trim();
+//        if (StringUtils.isNotBlank( all )){
+//            String [] namesplit = all.split( "," );
+//            String lastname = namesplit[ 0 ];   //1st element in array is always the last name
+//            return lastname;
+//        }
+//        return null;
+//    }
 
-    public static String getFirstName (Row row) {
-        String all = Parsers.getAsString( row.getAs( "InmateName" ) ).trim();
+
+    public static String getFirstName (Object obj) {
+        String all = Parsers.getAsString( obj );
+        if (all == null) return null;
+        all = all.trim();
+
         if ( StringUtils.isNotBlank( all ) ) {
             String[] namesplit = all.split( "," );
-            String firstname = namesplit[ 1 ];      //2nd element in array is always firstname
+            String firstname = namesplit[ 1 ].trim();      //2nd element in array is always firstname
 
             String[] firstname2 = firstname.split( " " );
 
@@ -141,13 +185,18 @@ public class MinPennHearings {
         return null;
     }
 
-    public static String getMiddleName (Row row) {
-        String all = Parsers.getAsString(row.getAs( "InmateName" )).trim();
+
+    public static String getMiddleName (Object obj) {
+        String all = Parsers.getAsString( obj );
+        if (all == null) return null;
+        all = all.trim();
         if (StringUtils.isNotBlank( all )){
             String [] namesplit = all.split( "," );
 
             String firstmid = namesplit[ 1 ];      //2nd element in array is always first/middle name
+            firstmid = firstmid.trim();
             String [] firstmid2 = firstmid.split( " " );
+
 
             if (firstmid2.length > 1) {  //if 2 words, last one 1 is the middle name
                 String middle = firstmid2[firstmid2.length-1];
