82f3c7c9c0b7ac1d951603fc56c2f8673f5b2155
Katherine Bernstein <katherine@openlattice.com>
2019-02-22 18:32:48 -0800

    fix arg count

--- src/main/java/com/openlattice/integrations/pennington/IntegrationRunner.java
+++ src/main/java/com/openlattice/integrations/pennington/IntegrationRunner.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class IntegrationRunner {
 
     public static void main( String[] args ) throws InterruptedException, IOException {
-        if ( args.length > 4 ) {
+        if ( args.length > 3 ) {
             ZuercherArrest.integrate( args );
         } else {
             MinPennHearings.integrate( args );
