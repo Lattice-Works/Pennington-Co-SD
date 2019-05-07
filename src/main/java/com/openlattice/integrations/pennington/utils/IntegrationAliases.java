0efe49f0d64c88bf55cacab8d2e2360e6ba57c6f
Todd Bergman <todd@openlattice.com>
2019-05-07 08:50:42 -0700

    Created IntegrationAliases in utils for Pennington-Co-SD

---
+++ src/main/java/com/openlattice/integrations/pennington/utils/IntegrationAliases.java
@@ -0,0 +1,111 @@
+package com.openlattice.integrations.pennington.utils;
+
+public class IntegrationAliases {
+
+    public static final String ADDRESS_ALIAS                        = "address";
+    public static final String APPEARS_IN_ALIAS                     = "appearsin";
+    public static final String APPEARS_IN_HEARING_ALIAS             = "appearsInHearing";
+    public static final String ARRESTED_IN_ALIAS                    = "arrestedin";
+    public static final String ARRESTEE_ALIAS                       = "arrestee";
+    public static final String CASE_ALIAS                           = "case";
+    public static final String CHARGE_ALIAS                         = "charge";
+    public static final String CHARGED_WITH_ALIAS                   = "chargedwith";
+    public static final String CONTACT_INFO_ALIAS                   = "contactInfo";
+    public static final String COUNTY_ALIAS                         = "county";
+    public static final String COURTHOUSE_ALIAS                     = "courthouse";
+    public static final String COURTROOM_ALIAS                      = "courtroom";
+    public static final String HAS_CONTACT_ALIAS                    = "hasContact";
+    public static final String HEARING_ALIAS                        = "hearing";
+    public static final String HEARING_APPEARS_IN_COUNTY_ALIAS      = "hearingAppearsInCounty";
+    public static final String HEARING_APPEARS_IN_COURTROOM_ALIAS   = "hearingAppearsInCourtroom";
+    public static final String HEARING_APPEARS_IN_COURTHOUSE_ALIAS  = "hearingAppearsInCourthouse";
+    public static final String INCIDENT_ALIAS                       = "incident";
+    public static final String JUDGE_ALIAS                          = "judge";
+    public static final String LIVES_AT_ALIAS                       = "livesat";
+    public static final String PERSON_ALIAS                         = "person";
+    public static final String PRETRIAL_CASE_ALIAS                  = "pretrialcase";
+    public static final String OVERSEES_CASE_ALIAS                  = "overseescase";
+    public static final String OVERSEES_HEARING_ALIAS               = "overseeshearing";
+
+
+    public static final String ABBREVIATION_COL             = "Abbreviation";
+    public static final String ADDRESS_COL                  = "Address";
+    public static final String AGE_AT_ARREST_COL            = "Age When Arrested";
+    public static final String ALCOHOL_USED_COL             = "Offender(s) Used Alcohol in Crime";
+    public static final String ALIASES_COL                  = "Aliases";
+    public static final String ARREST_DATETIME_COL          = "Arrest Date/Time";
+    public static final String ARREST_TRANSACTION_NO_COL    = "Arrest Transaction number";
+
+    public static final String CASE_NO_COL                  = "CaseNbr";
+    public static final String CASE_NUMBER_COL              = "Case Number";
+    public static final String CHARGE_COUNT_COL             = "ChargeCount";
+    public static final String CHARGE_NO_COL                = "ChargeNumber";
+    public static final String CITY_COL                     = "City";
+    public static final String COMPUTER_USED_COL            = "Offender(s) Used Computer Equipment";
+    public static final String COURTROOM_COL                = "Courtroom";
+
+    public static final String DOB_COL                      = "DtDOB";
+    public static final String DOCKET_NO_COL                = "DocketNumber";
+    public static final String DRUGS_USED_COL               = "Offender(s) Used Drugs in Crime";
+
+    public static final String ETHNICITY_COL                = "EthnicKy";
+    public static final String EYE_COL                      = "EyeKy";
+
+    public static final String FELONY_COL                   = "FelonyM1Cntr";
+    public static final String FIRST_NAME_COL               = "NameFirst";
+
+    public static final String GANG_ACTIVITY_COL            = "Other Gang";
+    public static final String GENDER_COL                   = "GenderKy";
+    public static final String GENERAL_ID_COL               = "ID";
+
+    public static final String HEIGHT_COL                   = "HtInches";
+    public static final String HEARING_COMMENT_COL          = "HearingComment";
+    public static final String HEARING_DATE_COL             = "HearingDate";
+    public static final String HEARING_TIME_COL             = "HearingTime";
+    public static final String HEARING_TYPE_COL             = "HearingType";
+
+    public static final String INCIDENT_START_DATETIME_COL  = "Incident Start Date/Time";
+    public static final String INMATE_FIRST_NAME_COL        = "InmateDOB";
+    public static final String INMATE_DOB_COL               = "InmateFName";
+    public static final String INMATE_LAST_NAME_COL         = "InmateLName";
+    public static final String INMATE_MIDDLE_NAME_COL       = "InmateMidName";
+    public static final String INMATE_SUFFIX_NAME_COL       = "InmateSfxName";
+    public static final String IS_MOBILE_COL                = "isMobile";
+    public static final String IS_PREFERRED_COL             = "preferred";
+
+    public static final String JACKET_NO_COL                = "Jacket number";
+    public static final String JUDGE_ID_COL                 = "JudgeID";
+    public static final String JUDICIAL_OFFICER_COL         = "JudicialOfficer";
+    public static final String JUVENILE_GANG_COL            = "Juvenile Gang";
+
+    public static final String LAST_NAME_COL                = "NameLast";
+
+    public static final String MIDDLE_NAME_COL              = "NameMid";
+
+    public static final String NAME_COL                     = "Name";
+
+    public static final String OFFENSE_COUNT_COL            = "Offense Count";
+    public static final String OFFENSE_NOTES_COL            = "Offense Details";
+
+    public static final String PERSON_ID_COL                = "PartyID";
+    public static final String PHONE_COL                    = "Phone";
+
+    public static final String RACE_COL                     = "Race";
+    public static final String RACE_KEY_COL                 = "RaceKy";
+    public static final String REPORTED_DATETIME_COL        = "Reported Date/Time";
+
+    public static final String SEALED_COL                   = "SealedCntr";
+    public static final String SEX_COL                      = "Sex";
+    public static final String SSN_COL                      = "SSN";
+    public static final String STATE_COL                    = "State";
+    public static final String STATUTE_COL                  = "Statute/Offense";
+    public static final String SUFFIX_NAME_COL              = "NameSfxKy";
+
+    public static final String UPDATE_TYPE_COL              = "UpdateType";
+
+    public static final String WEIGHT_COL                   = "WtLbs";
+
+    public static final String ZIP_COL                      = "ZIP";
+
+
+}
