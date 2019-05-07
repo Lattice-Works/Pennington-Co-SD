e03fb79d067351e597b48d9edb7b8e2b1d18386b
Todd Bergman <todd@openlattice.com>
2019-05-07 08:43:37 -0700

    Replaced strings with consts from EdmConstants and IntegrationAliases in CasesDailyDump

--- src/main/java/com/openlattice/integrations/pennington/OdysseyCasesDailyDump.java
+++ src/main/java/com/openlattice/integrations/pennington/OdysseyCasesDailyDump.java
@@ -3,6 +3,8 @@ package com.openlattice.integrations.pennington;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Maps;
 import com.openlattice.client.RetrofitFactory;
+import com.openlattice.integrations.pennington.utils.EdmConstants;
+import com.openlattice.integrations.pennington.utils.IntegrationAliases;
 import com.openlattice.shuttle.Flight;
 import com.openlattice.shuttle.MissionControl;
 import com.openlattice.shuttle.dates.DateTimeHelper;
@@ -29,51 +31,6 @@ public class OdysseyCasesDailyDump {
     private static final DateTimeHelper bdHelper = new DateTimeHelper( DateTimeZone.forOffsetHours( -6 ),
             "yyyy-MM-dd HH:mm:ss" );
 
-    private static final String PERSON_ENTITY_SET_NAME     = "southdakotapeople";
-    private static final String CASES_ENTITY_SET_NAME      = "southdakotapretrialcaseprocessings";
-    private static final String ADDRESSES_ENTITY_SET_NAME  = "southdakotaaddresses";
-    private static final String APPEARS_IN_ENTITY_SET_NAME = "southdakotaappearsin";
-    private static final String LIVES_AT_ENTITY_SET_NAME   = "southdakotalivesat";
-
-    private static final String PERSON_ID_FQN   = "nc.SubjectIdentification";
-    private static final String FIRST_NAME_FQN  = "nc.PersonGivenName";
-    private static final String LAST_NAME_FQN   = "nc.PersonSurName";
-    private static final String MIDDLE_NAME_FQN = "nc.PersonMiddleName";
-    private static final String SUFFIX_FQN      = "nc.PersonSuffix";
-    private static final String DOB_FQN         = "nc.PersonBirthDate";
-    private static final String RACE_FQN        = "nc.PersonRace";
-    private static final String ETHNICITY_FQN   = "nc.PersonEthnicity";
-    private static final String EYE_FQN         = "nc.PersonEyeColorText";
-    private static final String HEIGHT_FQN      = "nc.PersonHeightMeasure";
-    private static final String WEIGHT_FQN      = "nc.PersonWeightMeasure";
-    private static final String GENDER_FQN      = "nc.PersonSex";
-    private static final String CASE_NO_FQN     = "j.CaseNumberText";
-    private static final String ADDRESS_FQN     = "location.Address";
-    private static final String STRING_ID_FQN   = "general.stringid";
-    private static final String DATETIME_FQN    = "general.datetime";
-
-    private static final String PERSON_ID_COL   = "PartyID";
-    private static final String NAME_COL        = "Name";
-    private static final String FIRST_NAME_COL  = "NameFirst";
-    private static final String MIDDLE_NAME_COL = "NameMid";
-    private static final String LAST_NAME_COL   = "NameLast";
-    private static final String SUFFIX_NAME_COL = "NameSfxKy";
-    private static final String DOB_COL         = "DtDOB";
-    private static final String ADDRESS_COL     = "Address";
-    private static final String RACE_COL        = "RaceKy";
-    private static final String ETHNICITY_COL   = "EthnicKy";
-    private static final String EYE_COL         = "EyeKy";
-    private static final String HEIGHT_COL      = "HtInches";
-    private static final String WEIGHT_COL      = "WtLbs";
-    private static final String GENDER_COL      = "GenderKy";
-    private static final String CASE_NO_COL     = "CaseNbr";
-    private static final String FELONY_COL      = "FelonyM1Cntr";
-    private static final String SEALED_COL      = "SealedCntr";
-
-    private static final String PERSON_ALIAS  = "person";
-    private static final String CASE_ALIAS    = "case";
-    private static final String ADDRESS_ALIAS = "address";
-
     private static final SimpleDateFormat format = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
 
     public static void integrate( String[] args ) throws InterruptedException, IOException {
@@ -92,49 +49,49 @@ public class OdysseyCasesDailyDump {
         Flight flight = Flight.newFlight()
                 .createEntities()
 
-                .addEntity( PERSON_ALIAS )
-                .to( PERSON_ENTITY_SET_NAME )
-                .addProperty( PERSON_ID_FQN, PERSON_ID_COL )
-                .addProperty( FIRST_NAME_FQN, FIRST_NAME_COL )
-                .addProperty( MIDDLE_NAME_FQN, MIDDLE_NAME_COL )
-                .addProperty( LAST_NAME_FQN, LAST_NAME_COL )
-                .addProperty( SUFFIX_FQN, SUFFIX_NAME_COL )
-                .addProperty( DOB_FQN ).value( row -> bdHelper.parseDate( row.getAs( DOB_COL ) ) ).ok()
-                .addProperty( RACE_FQN, RACE_COL )
-                .addProperty( ETHNICITY_FQN, ETHNICITY_COL )
-                .addProperty( EYE_FQN, EYE_COL )
-                .addProperty( GENDER_FQN, GENDER_COL )
-                .addProperty( HEIGHT_FQN ).value( row -> Parsers.parseInt( row.getAs( HEIGHT_COL ) ) ).ok()
-                .addProperty( WEIGHT_FQN ).value( row -> Parsers.parseInt( row.getAs( WEIGHT_COL ) ) ).ok()
+                .addEntity( IntegrationAliases.PERSON_ALIAS )
+                .to( EdmConstants.PEOPLE_ENTITY_SET )
+                .addProperty( EdmConstants.PERSON_ID_FQN, IntegrationAliases.PERSON_ID_COL )
+                .addProperty( EdmConstants.FIRST_NAME_FQN, IntegrationAliases.FIRST_NAME_COL )
+                .addProperty( EdmConstants.MIDDLE_NAME_FQN, IntegrationAliases.MIDDLE_NAME_COL )
+                .addProperty( EdmConstants.LAST_NAME_FQN, IntegrationAliases.LAST_NAME_COL )
+                .addProperty( EdmConstants.SUFFIX_FQN, IntegrationAliases.SUFFIX_NAME_COL )
+                .addProperty( EdmConstants.DOB_FQN ).value( row -> bdHelper.parseDate( row.getAs( IntegrationAliases.DOB_COL ) ) ).ok()
+                .addProperty( EdmConstants.RACE_FQN, IntegrationAliases.RACE_KEY_COL )
+                .addProperty( EdmConstants.ETHNICITY_FQN, IntegrationAliases.ETHNICITY_COL )
+                .addProperty( EdmConstants.EYE_FQN, IntegrationAliases.EYE_COL )
+                .addProperty( EdmConstants.GENDER_FQN, IntegrationAliases.GENDER_COL )
+                .addProperty( EdmConstants.HEIGHT_FQN ).value( row -> Parsers.parseInt( row.getAs( IntegrationAliases.HEIGHT_COL ) ) ).ok()
+                .addProperty( EdmConstants.WEIGHT_FQN ).value( row -> Parsers.parseInt( row.getAs( IntegrationAliases.WEIGHT_COL ) ) ).ok()
                 .endEntity()
 
-                .addEntity( CASE_ALIAS )
-                .to( CASES_ENTITY_SET_NAME )
-                .entityIdGenerator( row -> row.get( CASE_NO_COL ).toString() )
-                .addProperty( CASE_NO_FQN, CASE_NO_COL )
+                .addEntity( IntegrationAliases.CASE_ALIAS )
+                .to( EdmConstants.CASES_ENTITY_SET )
+                .entityIdGenerator( row -> row.get( IntegrationAliases.CASE_NO_COL ).toString() )
+                .addProperty( EdmConstants.CASE_NO_FQN, IntegrationAliases.CASE_NO_COL )
                 .endEntity()
 
-                .addEntity( ADDRESS_ALIAS )
-                .to( ADDRESSES_ENTITY_SET_NAME )
-                .addProperty( ADDRESS_FQN, ADDRESS_COL )
+                .addEntity( IntegrationAliases.ADDRESS_ALIAS )
+                .to( EdmConstants.ADDRESSES_ENTITY_SET )
+                .addProperty( EdmConstants.ADDRESS_FQN, IntegrationAliases.ADDRESS_COL )
                 .endEntity()
 
                 .endEntities()
                 .createAssociations()
 
-                .addAssociation( "appearsin" )
-                .to( APPEARS_IN_ENTITY_SET_NAME )
-                .fromEntity( PERSON_ALIAS )
-                .toEntity( CASE_ALIAS )
-                .addProperty( STRING_ID_FQN, CASE_NO_COL )
-                .addProperty( DATETIME_FQN ).value( row -> bdHelper.parse( format.format( new Date() ) ) ).ok()
+                .addAssociation( IntegrationAliases.APPEARS_IN_ALIAS )
+                .to( EdmConstants.APPEARS_IN_ENTITY_SET )
+                .fromEntity( IntegrationAliases.PERSON_ALIAS )
+                .toEntity( IntegrationAliases.CASE_ALIAS )
+                .addProperty( EdmConstants.STRING_ID_FQN, IntegrationAliases.CASE_NO_COL )
+                .addProperty( EdmConstants.DATETIME_FQN ).value( row -> bdHelper.parse( format.format( new Date() ) ) ).ok()
                 .endAssociation()
 
-                .addAssociation( "livesat" )
-                .to( LIVES_AT_ENTITY_SET_NAME )
-                .fromEntity( PERSON_ALIAS )
-                .toEntity( ADDRESS_ALIAS )
-                .addProperty( STRING_ID_FQN, PERSON_ID_COL )
+                .addAssociation( IntegrationAliases.LIVES_AT_ALIAS )
+                .to( EdmConstants.LIVES_AT_ENTITY_SET )
+                .fromEntity( IntegrationAliases.PERSON_ALIAS )
+                .toEntity( IntegrationAliases.ADDRESS_ALIAS )
+                .addProperty( EdmConstants.STRING_ID_FQN, IntegrationAliases.PERSON_ID_COL )
                 .endAssociation()
 
                 .endAssociations()
