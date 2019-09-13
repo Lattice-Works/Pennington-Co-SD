97df248ceadaaa409670a7872c3d4a9160e4b480
Todd Bergman <todd@openlattice.com>
2019-09-13 11:37:04 -0700

    Created Charge consts

---
+++ src/main/java/com/openlattice/integrations/pennington/utils/ChargeConstants.java
@@ -0,0 +1,26 @@
+package com.openlattice.integrations.pennington.utils;
+
+public class ChargeConstants {
+
+    // ALIASES
+    public static final String COUNTY_ALIAS             = "county";
+    public static final String COURTHOUSE_ALIAS         = "courthouse";
+    public static final String COURTROOM_ALIAS          = "courtroom";
+    public static final String APPEARS_IN_ALIAS         = "appearsin";
+    public static final String CONTACT_INFO_GIVEN_ALIAS = "contactinfogiven";
+    public static final String AGENCY_ALIAS             = "agency";
+    public static final String PHONE_ALIAS              = "phone";
+
+    public static final String ID           = "id";
+    public static final String NAME         = "name";
+    public static final String ADDRESS      = "address";
+    public static final String IS_VIOLENT   = "voilent";
+    public static final String STATUTE      = "statute";
+    public static final String DESCRIPTION  = "description";
+    public static final String STEP_2       = "step2";
+    public static final String STEP_4       = "step4";
+    public static final String BHE          = "bhe";
+    public static final String BRE          = "bre";
+    public static final String DEGREE       = "degree";
+    public static final String DEGREE_SHORT = "degreeShort";
+}
